4d32e72a5843dc63b0fb43352a94199d864a6013
Hugues Demers <hdemers@gmail.com>
2012-05-11 14:39:20 -0400

    Add bigquery calls.
    Does not work yet. Seems to me there is a mismatch between the project id,
    which is Michel's project id, and my authorization. However, even when changing
    to my project id, it does not work, cause I can't de-authorize my app (or I
    haven't found out how).

--- bigquery.js
+++ bigquery.js
@@ -1,11 +1,48 @@
-/*global  */
+/*global  gapi:false*/
 define([
-  "jquery"
+  "jquery",
+  "gapi"
 ],
 function ($) {
-  var exports = {};
   console.log("Initializing module bigquery.");
+  var exports = {},
+    projectId = "22498051859",
+    clientId = projectId + '.apps.googleusercontent.com',
+    apiKey = 'AIzaSyAHqTYilT1TJhwyWcXnyZzwxSdQAkr07g0',
+    scopes = 'https://www.googleapis.com/auth/bigquery',
+    defDataSet = {'projectId': projectId},
+    queryData = {
+      'projectId': projectId,
+      'query': 'SELECT repository_name ' + 
+        'FROM [publicdata:samples.github_timeline] LIMIT 100;',
+    };
 
+  function requestBigQuery() {
+    console.log("Making request to BigQuery.");
+    //var request = gapi.client.bigquery.projects.list();
+    var request = gapi.client.bigquery.jobs.query(queryData);
+    request.execute(function (response) {
+      console.log("Got ", response);
+    });
+  }
+
+  // TODO: Remove the need to timeout. Find a way to call back from the gapi
+  // 'module' loading. Read require.js doc.
+  setTimeout(function () {
+    // The API key auth mecanism does not work, even though the doc says so.
+    // Must use OAuth2, which is a pain.
+    //gapi.client.setApiKey(apiKey); 
+    
+    gapi.auth.authorize({
+      client_id: clientId, 
+      scope: scopes
+    }, function () {
+      console.log("Login complete. Token: ", gapi.auth.getToken());
+      gapi.client.load('bigquery', 'v2', requestBigQuery);
+    });
+
+    console.log("BigQuery API key set.");
+  }, 1000);
 
   return exports;
 });
