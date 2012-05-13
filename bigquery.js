8facbb4987bc6e20794404e9ec17a4fc9b285a6b
Hugues Demers <hdemers@gmail.com>
2012-05-13 18:31:28 -0400

    Fix big queries.
    Big queries can now be done. Set your project id properly (see the URL of your
    Google API console's project.) and activate OAuth2 (again in your Google API
    console) by making sure the Javascript origins include 'http://localhost:8000'
    or whatever URL you're hosting at.

--- bigquery.js
+++ bigquery.js
@@ -1,4 +1,4 @@
-/*global  gapi:false*/
+/*global  gapi:false, viewmodel:false, _:false*/
 define([
   "jquery",
   "gapi"
@@ -6,42 +6,57 @@ define([
 function ($) {
   console.log("Initializing module bigquery.");
   var exports = {},
-    projectId = "22498051859",
+    // NOTE: Put your project id here. See the URL of your Google API console.
+    projectId = "",
     clientId = projectId + '.apps.googleusercontent.com',
-    apiKey = 'AIzaSyAHqTYilT1TJhwyWcXnyZzwxSdQAkr07g0',
     scopes = 'https://www.googleapis.com/auth/bigquery',
-    defDataSet = {'projectId': projectId},
-    queryData = {
-      'projectId': projectId,
-      'query': 'SELECT repository_name ' + 
-        'FROM [publicdata:samples.github_timeline] LIMIT 100;',
-    };
+    mostForkedRepoQuery = "SELECT repository_name, " + 
+        "MAX(repository_forks) as maxi " +
+        "FROM githubarchive:github.timeline " +
+        "WHERE type = 'ForkEvent' AND " +
+        "LENGTH(actor_attributes_location) > 3 AND " +
+        "repository_forks > 200 " +
+        "GROUP BY repository_name " +
+        "ORDER BY maxi DESC " +
+        "LIMIT 20",
+    testQuery = 'SELECT repository_name ' + 
+        'FROM githubarchive:github.timeline LIMIT 100;';
 
-  function requestBigQuery() {
+  if (projectId === "") {
+    alert("You did not set your project id.");
+    return {};
+  }
+
+  exports.request = function (query, callback) {
     console.log("Making request to BigQuery.");
-    //var request = gapi.client.bigquery.projects.list();
-    var request = gapi.client.bigquery.jobs.query(queryData);
-    request.execute(function (response) {
-      console.log("Got ", response);
+    var request = gapi.client.bigquery.jobs.query({
+      'projectId': projectId,
+      'query': query,
+    });
+    request.execute(callback || function (response) {
+      console.log("Got: ", response);
     });
   }
 
   // TODO: Remove the need to timeout. Find a way to call back from the gapi
   // 'module' loading. Read require.js doc.
   setTimeout(function () {
-    // The API key auth mecanism does not work, even though the doc says so.
-    // Must use OAuth2, which is a pain.
-    //gapi.client.setApiKey(apiKey); 
-    
     gapi.auth.authorize({
       client_id: clientId, 
       scope: scopes
     }, function () {
       console.log("Login complete. Token: ", gapi.auth.getToken());
-      gapi.client.load('bigquery', 'v2', requestBigQuery);
+      gapi.client.load('bigquery', 'v2', function () {
+        exports.request(mostForkedRepoQuery, function (result) {
+          var names = _.map(result.rows, function (row) {
+            return row.f[0].v;
+          });
+          console.log("Most forked repos: ", names);
+          viewmodel.repositories(names);
+          viewmodel.selectedRepo(names[0]);
+        });
+      });
     });
-
-    console.log("BigQuery API key set.");
   }, 1000);
 
   return exports;
