2a842728019030623f753bee9933ca57c2aa0a9e
Hugues Demers <hdemers@gmail.com>
2012-05-15 10:25:18 -0400

    Coding standard and minor tweeks.
    Normalize the indentation to 2 spaces and use the coding standard that
    seems most applied on the net. See for started [Crockford
    conventions](http://javascript.crockford.com/code.html), although most people
    use 2 spaces and not 4 as Crockford does.
    
    Modify the response handling by using the totalResultsCount.
    TODO: Handle the no result case.

--- geonames.js
+++ geonames.js
@@ -3,40 +3,42 @@ define([
   "jquery"
 ],
 function ($) {
-  var exports = {},
-  	url = "http://api.geonames.org/searchJSON?";
   console.log("Initializing module geonames.");
+  var exports = {},
+    url = "http://api.geonames.org/searchJSON?";
 
   exports.getLngLat = function (user, locationName, callback) {
-  	//Build request ajax object
-	console.log("flag");
-	
-  	$.ajax({
-  		  url: url,
-  		  data: {
-  			q: locationName,
-  			maxRows: 1,
-  			username: user
-  		  },
-  		  dataType: "text" //Return data is cast as text so as to avoid safety issues with evaluating unknown js code.
-  		})
-  		.done(function(msg){
-	  		//Safely parse JSON response.
-	  		var jsonResp = JSON.parse(msg);
-	  		console.log(jsonResp);
-	  		//Navigate the response.
-	  		$.each(jsonResp, function(key, val) {
-	  			if(key == "geonames"){
-	  				var name = val[0]["toponymName"];
-	  				var lat = val[0]["lat"];
-	  				var long = val[0]["lng"];
-	  				
-	  				console.log(name+"\t"+lat+"\t"+long);
-	  				callback(name, long, lat);
-	  			};
-	  		});
-  		});
+    // Build request ajax object
+    console.log("getLngLat called.");
+    $.ajax({
+      url: url,
+      data: {
+        q: locationName,
+        maxRows: 1,
+        username: user
+      },
+      // Returned data is cast as text so as to avoid safety issues with
+      // evaluating unknown js code.
+      dataType: "text" 
+    }).done(function (msg) {
+      // Safely parse JSON response.
+      var jsonResp = JSON.parse(msg), name, lat, lng;
+      console.log(jsonResp);
+      // If we have at least one result, use the first element of the returned
+      // array (we asked for only one, there shouldn't be more).
+      if (jsonResp.totalResultsCount !== 0) {
+        name = jsonResp.geonames[0].toponymName;
+        lat = jsonResp.geonames[0].lat;
+        lng = jsonResp.geonames[0].lng;
+        console.log("Got results from geonames: ", name, lat, lng);
+        callback(name, lng, lat);
+      }
+      else {
+        // What should we do if there is no result?
+        console.warn("No results.");
+      }
+    });
   };
-  
+
   return exports;
 });
